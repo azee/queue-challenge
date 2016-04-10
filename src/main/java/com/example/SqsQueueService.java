6711202f7658ad69b495503a639897c28c312bd7
Aziz Namazov <azee@azee.local>
2016-04-10 22:45:11 +0300

    Add sqs

--- src/main/java/com/example/SqsQueueService.java
+++ src/main/java/com/example/SqsQueueService.java
@@ -1,7 +1,13 @@
 package com.example;
 
 import com.amazonaws.services.sqs.AmazonSQSClient;
+import com.amazonaws.services.sqs.model.QueueAttributeName;
+import com.amazonaws.services.sqs.model.ReceiveMessageRequest;
 import com.example.beans.Message;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
 
 public class SqsQueueService extends BaseQueueService {
   //
@@ -12,37 +18,69 @@ public class SqsQueueService extends BaseQueueService {
   // terms of how well they map to the implementation intended for a production environment.
   //
 
+  private AmazonSQSClient sqsClient;
+
 
   @Override
   public void push(String queueName, Message message) {
-
+    sqsClient.sendMessage(sqsClient.getQueueUrl(queueName).getQueueUrl(), serializeMessage(message));
   }
 
   @Override
   public Message pull(String queueName) {
-    return null;
+    ReceiveMessageRequest receiveMessageRequest = new ReceiveMessageRequest(sqsClient.getQueueUrl(queueName).getQueueUrl());
+    receiveMessageRequest.setMaxNumberOfMessages(1);
+    List<com.amazonaws.services.sqs.model.Message> messages = sqsClient.receiveMessage(queueName).getMessages();
+    return messages.size() < 1 ? null : convert(messages.get(0));
   }
 
   @Override
   public void delete(String queueName, Message message) {
-
+    //noop - no need to delete from pending - resend is implemented inside sqs
   }
 
   @Override
   public long messagesInQueue(String queueName) {
-    return 0;
+    return Long.parseLong(sqsClient.getQueueAttributes(queueName, Arrays.asList(QueueAttributeName.ApproximateNumberOfMessages.toString()))
+            .getAttributes().get(QueueAttributeName.ApproximateNumberOfMessages.toString()));
+
+
   }
 
   @Override
   public long pendingMessages(String queueName) {
-    return 0;
-  }
+    Map<String, String> attrs = sqsClient.getQueueAttributes(queueName,
+            Arrays.asList(QueueAttributeName.ApproximateNumberOfMessagesNotVisible.toString(),
+                    QueueAttributeName.ApproximateNumberOfMessagesDelayed.toString())).getAttributes();
+    return Long.parseLong(attrs.get(QueueAttributeName.ApproximateNumberOfMessagesNotVisible.toString())) +
+            Long.parseLong(attrs.get(QueueAttributeName.ApproximateNumberOfMessagesDelayed.toString()));
 
-  public SqsQueueService(AmazonSQSClient sqsClient) {
   }
 
   @Override
   protected void clearPending() {
-
+    //noop - no need to delete from pending - resend is implemented inside sqs
+    //we won't even start a watchdog thread
   }
+
+  public SqsQueueService(AmazonSQSClient sqsClient) {
+    this.sqsClient = sqsClient;
+  }
+
+  private String serializeMessage(Message message){
+    //ToDo: add Jackson or ru.greatbit.java-utils to implement serialization to JSON
+    return message.toString();
+  }
+
+  private String deserializeMessage(Message message){
+    //ToDo: add Jackson or ru.greatbit.java-utils to implement serialization to JSON
+    return message.toString();
+  }
+
+  private Message convert(com.amazonaws.services.sqs.model.Message message) {
+    //ToDo: add Jackson or ru.greatbit.java-utils to implement deSerialization to JSON
+    return new Message(message.getBody());
+  }
+
+
 }
